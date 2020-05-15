afdd5816b10a81c6811052e044dfef94d8d34939
freaky1310@gmail.com <arthas1310>
2020-05-15 11:22:28 +0300

    Random cropping implemented

--- dataset.py
+++ dataset.py
@@ -1,8 +1,10 @@
 import torchvision.transforms as transforms
-import os
+import os, random
 
 from torch.utils.data import Dataset
 from PIL import Image
+
+from utils import random_crop
 
 class COCO(Dataset):
 
@@ -14,12 +16,14 @@ class COCO(Dataset):
 
     def __getitem__(self, index):
 
-        image = Image.open(self.image_paths + os.listdir(self.image_paths)[index])
-        target = Image.open(self.target_paths + os.listdir(self.target_paths)[index])
+        image, target = random_crop(Image.open(self.image_paths + os.listdir(self.image_paths)[index]),
+                                    Image.open(self.target_paths + os.listdir(self.target_paths)[index]),
+                                    image_max_range=32,
+                                    target_scale=4)
         t_image = self.transforms(image)
         t_target = self.transforms(target)
         return t_image, t_target
 
     def __len__(self):  # return count of sample we have
 
-        return len(self.image_paths)
+        return len(os.listdir(self.image_paths))
---
+++ utils.py
@@ -0,0 +1,19 @@
+import random
+
+from PIL import Image
+
+# Given that I have to perform the same random crop on both the image and the target, I had to redefine the function
+def random_crop(image, target, image_max_range=32, target_scale=4):
+    crop_x = random.randint(0, image_max_range)
+    crop_y = random.randint(0, image_max_range)
+    image = image.crop(crop_x,
+                       crop_y,
+                       crop_x + image_max_range,
+                       crop_y + image_max_range
+                       )
+    target = target.crop(crop_x * target_scale,
+                         crop_y * target_scale,
+                         (crop_x + image_max_range) * target_scale,
+                         (crop_y + image_max_range) * target_scale
+                         )
+    return image, target
