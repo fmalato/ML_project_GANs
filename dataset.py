89ba419d287bbe404bbf603b97e62ddfb2185903
freaky1310@gmail.com <arthas1310>
2020-05-15 19:47:51 +0300

    Let's try not to random_crop

--- dataset.py
+++ dataset.py
@@ -4,7 +4,7 @@ import os, random
 from torch.utils.data import Dataset
 from PIL import Image
 
-from utils import random_crop
+from utils import random_crop, square_patch
 
 class COCO(Dataset):
 
@@ -16,10 +16,15 @@ class COCO(Dataset):
 
     def __getitem__(self, index):
 
-        image, target = random_crop(Image.open(self.image_paths + os.listdir(self.image_paths)[index]),
+        """image, target = random_crop(Image.open(self.image_paths + os.listdir(self.image_paths)[index]),
                                     Image.open(self.target_paths + os.listdir(self.target_paths)[index]),
                                     image_max_range=32,
-                                    target_scale=4)
+                                    target_scale=4)"""
+        #image = Image.open(self.image_paths + os.listdir(self.image_paths)[index])
+        #target = Image.open(self.target_paths + os.listdir(self.target_paths)[index])
+        p = random.randint(0, 3)
+        image = square_patch(self.image_paths + os.listdir(self.image_paths)[index], 32)[p]
+        target = square_patch(self.target_paths + os.listdir(self.target_paths)[index], 128)[p]
         t_image = self.transforms(image)
         t_target = self.transforms(target)
         return t_image, t_target
