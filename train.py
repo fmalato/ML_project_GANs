0e7b2b4671e21a9d90468c0b0c0cf6ef87dbd4c2
freaky1310@gmail.com <arthas1310>
2020-06-13 10:39:33 +0300

    loss A fix

--- train.py
+++ train.py
@@ -67,7 +67,7 @@ def multiple_train(net, loss_type, optimizer, device, epochs, batch_size=1, inte
 
             loss = Tensor(np.zeros(1)).cuda()
             output = net(images.float().to(device), bicub.float().to(device))
-            output = torch.add(output.to(device), bicub.to(device))
+            output = torch.add(torch.add(output.to(device), PCM.to(device)), bicub.to(device))
 
             for criterion in criterions:
                 if criterion == LossP:
