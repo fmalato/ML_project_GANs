1d9a64ffa27a19cd1c08a0a002debe04516e4eb2
Federico Malato <fm7017325@localhost.dinfo.unifi.it>
2020-06-18 18:52:48 +0200

    parameters

--- losses.py
+++ losses.py
@@ -71,36 +71,48 @@ def LossT(vgg, device, image, target, patch_size=16):
     image = torch.split(image, 1, dim=0)
     target = torch.split(target, 1, dim=0)
 
+    vgg_1 = vgg[0].cuda()
+    vgg_2 = vgg[1].cuda()
+    vgg_3 = vgg[2].cuda()
+    
     patches = []
     patches_target = []
     batch_size = int(image[0].shape[2] / patch_size) ** 2
+    idx = 0
     for el in image:
-        new = el.unfold(1, 3, 3).unfold(2, patch_size, patch_size).unfold(3, patch_size, patch_size)
-        new = new.reshape((batch_size, 3, patch_size, patch_size))
-        new = torch.split(new, 1, dim=0)
-        patches += new
+        # one patch every 4 is computed in order to reduce computation. On the dataset there are 655114*64 16x16 patches
+        # so I guess an eighth (kind of 5kk)  of them is a good trade-off for a 15h speed up on the training
+        if idx % 8 == 0:
+            new = el.unfold(1, 3, 3).unfold(2, patch_size, patch_size).unfold(3, patch_size, patch_size)
+            new = new.reshape((batch_size, 3, patch_size, patch_size))
+            new = torch.split(new, 1, dim=0)
+            patches += new
+        idx += 1
     del image
+    idx = 0
     for el in target:
-        new = el.unfold(1, 3, 3).unfold(2, patch_size, patch_size).unfold(3, patch_size, patch_size)
-        new = new.reshape((batch_size, 3, patch_size, patch_size))
-        new = torch.split(new, 1, dim=0)
-        patches_target += new
+        if idx % 8 == 0:
+            new = el.unfold(1, 3, 3).unfold(2, patch_size, patch_size).unfold(3, patch_size, patch_size)
+            new = new.reshape((batch_size, 3, patch_size, patch_size))
+            new = torch.split(new, 1, dim=0)
+            patches_target += new
     del target
+    del idx
 
     loss_1 = torch.Tensor(np.zeros(1)).cuda()
     loss_2 = torch.Tensor(np.zeros(1)).cuda()
     loss_3 = torch.Tensor(np.zeros(1)).cuda()
 
     for i, t in zip(patches, patches_target):
-        loss_1 += torch.mean((gram_matrix(vgg[0](i)) - gram_matrix(vgg[0](t))) ** 2)
-        loss_2 += torch.mean((gram_matrix(vgg[1](i)) - gram_matrix(vgg[1](t))) ** 2)
-        loss_3 += torch.mean((gram_matrix(vgg[2](i)) - gram_matrix(vgg[2](t))) ** 2)
+        loss_1 += torch.mean((gram_matrix(vgg_1(i)) - gram_matrix(vgg_1(t))) ** 2)
+        loss_2 += torch.mean((gram_matrix(vgg_2(i)) - gram_matrix(vgg_2(t))) ** 2)
+        loss_3 += torch.mean((gram_matrix(vgg_3(i)) - gram_matrix(vgg_3(t))) ** 2)
 
     loss_1 = loss_1.mean().item()
     loss_2 = loss_2.mean().item()
     loss_3 = loss_3.mean().item()
 
-    return (3e-7 * loss_1 + 1e-6 * loss_2 + 1e-6 * loss_3).cuda()
+    return 3e-7 * loss_1 + 1e-6 * loss_2 + 1e-6 * loss_3
 
 
 
