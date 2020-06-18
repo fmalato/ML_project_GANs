de2cc2a5d1715bdebdddbf3c28afd66ad43fcf8e
freaky1310@gmail.com <arthas1310>
2020-06-18 16:10:35 +0300

    Texture loss 2.0

--- losses.py
+++ losses.py
@@ -15,18 +15,18 @@ def LossE(device, image, target):
 
 """ Perceptual Loss """
 def LossP(vgg, device, image, target):
-    criterion = nn.MSELoss().cuda()
+
     vgg_2 = vgg[0].float()
     vgg_5 = vgg[1].float()
     vgg_2.cuda()
     vgg_5.cuda()
 
-    extr_feat = vgg_2(image.float())
-    real_feat = vgg_2(target.float())
-    loss_2 = criterion(extr_feat, real_feat.detach())
-    extr_feat = vgg_5(image.float())
-    real_feat = vgg_5(target.float())
-    loss_5 = criterion(extr_feat, real_feat.detach())
+    extr_feat = vgg_2(image)
+    real_feat = vgg_2(target)
+    loss_2 = torch.mean((extr_feat - real_feat) ** 2)
+    extr_feat = vgg_5(image)
+    real_feat = vgg_5(target)
+    loss_5 = torch.mean((extr_feat - real_feat) ** 2)
 
     return ((2e-1)*loss_2 + (2e-2)*loss_5).cuda()
 
@@ -67,34 +67,38 @@ def LossA(discriminator, device, output_g, target, optim_d, last_dx, last_dgz, l
 
 """ Texture Loss """
 def LossT(vgg, device, image, target, patch_size=16):
-    criterion = nn.MSELoss()
-    vgg_1 = vgg[0].cuda()
-    vgg_2 = vgg[1].cuda()
-    vgg_3 = vgg[2].cuda()
     # Images
     image = torch.split(image, 1, dim=0)
-    img_size = image[0].shape[2]
-    batch_size = int(img_size / patch_size)**2
+    target = torch.split(target, 1, dim=0)
+
     patches = []
+    patches_target = []
+    batch_size = int(image[0].shape[2] / patch_size) ** 2
     for el in image:
         new = el.unfold(1, 3, 3).unfold(2, patch_size, patch_size).unfold(3, patch_size, patch_size)
         new = new.reshape((batch_size, 3, patch_size, patch_size))
-        patches.append(new)
-    patches = torch.cat(patches).to(device)
-    # Targets
-    img_size = target[0].shape[2]
-    batch_size = int(img_size / patch_size) ** 2
-    target = torch.split(target, 1, dim=0)
-    patches_target = []
+        new = torch.split(new, 1, dim=0)
+        patches += new
+    del image
     for el in target:
         new = el.unfold(1, 3, 3).unfold(2, patch_size, patch_size).unfold(3, patch_size, patch_size)
         new = new.reshape((batch_size, 3, patch_size, patch_size))
-        patches_target.append(new)
-    patches_target = torch.cat(patches_target).to(device)
-    # Computing loss
-    loss_1 = criterion(gram_matrix(vgg_1(patches)).float(), gram_matrix(vgg_1(patches_target.float())).float())
-    loss_2 = criterion(gram_matrix(vgg_2(patches)).float(), gram_matrix(vgg_2(patches_target.float())).float())
-    loss_3 = criterion(gram_matrix(vgg_3(patches)).float(), gram_matrix(vgg_3(patches_target.float())).float())
+        new = torch.split(new, 1, dim=0)
+        patches_target += new
+    del target
+
+    loss_1 = torch.Tensor(np.zeros(1)).cuda()
+    loss_2 = torch.Tensor(np.zeros(1)).cuda()
+    loss_3 = torch.Tensor(np.zeros(1)).cuda()
+
+    for i, t in zip(patches, patches_target):
+        loss_1 += torch.mean((gram_matrix(vgg[0](i)) - gram_matrix(vgg[0](t))) ** 2)
+        loss_2 += torch.mean((gram_matrix(vgg[1](i)) - gram_matrix(vgg[1](t))) ** 2)
+        loss_3 += torch.mean((gram_matrix(vgg[2](i)) - gram_matrix(vgg[2](t))) ** 2)
+
+    loss_1 = loss_1.mean().item()
+    loss_2 = loss_2.mean().item()
+    loss_3 = loss_3.mean().item()
 
     return (3e-7 * loss_1 + 1e-6 * loss_2 + 1e-6 * loss_3).cuda()
 
