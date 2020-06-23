4bab1d4d581d2522f3656ef1648e1b923ba0dcf5
freaky1310@gmail.com <arthas1310>
2020-06-23 21:48:07 +0300

    Allowing and denying training for real

--- losses.py
+++ losses.py
@@ -33,18 +33,16 @@ def LossP(device, extr_feat_2, true_feat_2, extr_feat_5, true_feat_5):
 def LossA(discriminator, device, output_g, target, optim_d, lossT=False, train_disc=True):
     batch_size = output_g.size(0)
     criterion = nn.BCELoss()
-    l_true = Variable(torch.full((batch_size,), 0.9, device=device), requires_grad=False)
-    l_fake = Variable(torch.full((batch_size,), 0.0, device=device), requires_grad=False)
-    l_true_g = Variable(torch.full((batch_size,), 1.0, device=device), requires_grad=False)
+    l_true = torch.full((batch_size,), random.uniform(0.9, 1.0), device=device)
+    l_fake = torch.full((batch_size,), random.uniform(0.0, 0.1), device=device)
+    l_true_g = torch.full((batch_size,), 1.0, device=device)
 
     # Discriminator
+    discriminator.allow_train()
     optim_d.zero_grad()
     output_t = discriminator(target).view(-1).clamp(1e-7, 1-1e-7)
     output_d = discriminator(output_g.detach()).view(-1).clamp(1e-7, 1 - 1e-7)
     d_x = output_t.mean().item()
-
-    if train_disc:
-        discriminator.allow_train()
     # loss_d = Variable(- 0.5 * torch.mean(torch.log(output_t)) - 0.5 * torch.mean(torch.log(l_true_g - output_d)), requires_grad=True)
     loss_d = criterion(output_d, l_fake) + criterion(output_t, l_true)
     if train_disc:
